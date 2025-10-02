68c9b455ad79244f80c9dda3e38a9d4a453dbbb6
Thiago-Nassiben-Freri <thiagonassibenfreri2@gmail.com>
2025-10-02 15:01:45 -0300

    First commit for my c game

---
+++ petrun.c
@@ -0,0 +1,181 @@
+#include "raylib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM_OBSTACLES 5
+
+// Cria o objeto obstaculo 
+typedef struct Obstacle {
+    float x;
+    float y;
+    float width;
+    float height;
+} Obstacle;
+
+// Reseta o estado do jogo, assim as variaveis voltam ao padrao original
+void ResetGame(float *xPos, float *yPos, float *velY, int *score, Obstacle obstacles[], int screenWidth, float groundY, int size) {
+    *xPos = 100;
+    *velY = 0;
+    *score = 0;
+    *yPos = groundY - size;  
+    
+    for (int i = 0; i < NUM_OBSTACLES; i++) {
+        obstacles[i].width = 20;
+        obstacles[i].height = 50;
+        obstacles[i].x = screenWidth + i * 750 + GetRandomValue(0, 200);
+        obstacles[i].y = groundY - obstacles[i].height;
+    }
+}
+
+// Tela inicial Start e Exit
+void StartGame(int screenWidth, int screenHeight) {
+    while (!IsKeyPressed(KEY_S)) {
+        BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("PetRun", screenWidth/2 - 160, screenHeight/2.5 - 70,40, WHITE);
+            DrawText("Start Game (S)", screenWidth/2 - 160, screenHeight/2.5,40, WHITE);
+            DrawText("Exit Game (E)", screenWidth/2 - 160, screenHeight/2.5 + 40, 40, WHITE);
+        EndDrawing();
+            
+        if (IsKeyPressed(KEY_E)) {
+            CloseWindow();
+        }
+    }
+} 
+
+int main(void) {
+    // Variaveis
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    
+    const float GRAVITY = 800;
+    const float JUMP = -490;
+    
+    float xPos = 100;
+    float yPos = 0;
+    float velY = 0;
+    
+    const int size = 40;
+    const float groundY = 300;
+    
+    int score = 0;
+    int hiscore = 0;
+    
+    Obstacle obstacles[NUM_OBSTACLES];
+    float obstacleSpeed = 200;
+    
+    srand(time(NULL));
+    yPos = groundY - size;
+    
+    // Gera obstaculos aleatorios
+    for (int i = 0; i < NUM_OBSTACLES; i++) {
+        obstacles[i].width = 20;
+        obstacles[i].height = 50;
+        obstacles[i].x = screenWidth + i * 750 + GetRandomValue(0, 200);
+        obstacles[i].y = groundY - obstacles[i].height;
+    }
+    
+    // Inicia a tela do jogo
+    InitWindow(screenWidth, screenHeight, "PetRun");
+    SetTargetFPS(60);
+    
+    bool gameOver = false;
+    ResetGame(&xPos, &yPos, &velY, &score, obstacles, screenWidth, groundY, size);
+    
+    // Tela de start e exit do jogo   
+    StartGame(screenWidth, screenHeight);
+    
+    // Deixa o jogo em loop ate ser fechado
+    while(!WindowShouldClose()){
+        float dt = GetFrameTime();
+        
+        if (!gameOver) {
+            // Pulo
+            if (IsKeyPressed(KEY_SPACE) && yPos >= groundY - size) {
+                velY = JUMP;
+            }
+            
+            // Gravidade
+            velY += GRAVITY * dt;
+            yPos += velY * dt;
+            
+            // Impede cair
+            if (yPos > groundY - size) {
+                yPos = groundY - size;
+                velY = 0;    
+            }
+            
+            // Quando pressionado a tecla R, o jogo reinicia
+            if (IsKeyPressed(KEY_R)) {
+                ResetGame(&xPos, &yPos, &velY, &score, obstacles, screenWidth, groundY, size);
+            }
+            
+            // Quando pressionado a tecla Q, a tela volta para o menu inicial e reinicia o jogo
+            if (IsKeyPressed(KEY_Q)) {
+                ResetGame(&xPos, &yPos, &velY, &score, obstacles, screenWidth, groundY, size);
+                StartGame(screenWidth, screenHeight);
+            }
+            
+            // Movimenta obstáculos
+            for (int i = 0; i < NUM_OBSTACLES; i++) {
+                obstacles[i].x -= obstacleSpeed * dt;
+                
+                if (obstacles[i].x + obstacles[i].width < 0) {
+                    obstacles[i].x = screenWidth + GetRandomValue(0, 200);
+                }
+                
+                // Checa colisão
+                if (CheckCollisionRecs(
+                    (Rectangle){ xPos, yPos, size, size },
+                    (Rectangle){ obstacles[i].x, obstacles[i].y, obstacles[i].width, obstacles[i].height }
+                )) {
+                    gameOver = true;
+                }
+            }
+            
+            // Score aumenta somente se não estiver em game over
+            score += 1;
+            
+            // Atualiza highscore
+            if (score > hiscore) {
+                hiscore = score;
+            }
+        }
+        
+        // Desenha
+        BeginDrawing();
+            ClearBackground(BLACK);
+            
+            if (!gameOver) {
+                DrawRectangle((int)xPos, (int)yPos, size, size, RED);
+                DrawLine(0, groundY, screenWidth, groundY, WHITE);
+                DrawText(TextFormat("Score: %d", score), 25, 20, 20, WHITE);
+                DrawText(TextFormat("High Score: %d", hiscore), 25, 50, 20, GREEN);
+                
+                for (int i = 0; i < NUM_OBSTACLES; i++) {
+                    DrawRectangle(
+                        (int)obstacles[i].x,
+                        (int)obstacles[i].y,
+                        (int)obstacles[i].width,
+                        (int)obstacles[i].height,
+                        GREEN
+                    );
+                }
+            } else {
+                DrawText("Game Over!", screenWidth/2 - 120, screenHeight/2.5, 40, RED);
+                DrawText("Press R to Restart", screenWidth/2 - 120, screenHeight/2.5 + 40, 20, WHITE);
+                DrawText(TextFormat("Your Score: %d", score), screenWidth/2 - 120, screenHeight/2.5 + 100, 20, BLUE);
+                DrawText(TextFormat("High Score: %d", hiscore), screenWidth/2 - 120, screenHeight/2.5 + 120, 20, GREEN);
+                
+                if (IsKeyPressed(KEY_R)) {
+                    ResetGame(&xPos, &yPos, &velY, &score, obstacles, screenWidth, groundY, size);
+                    gameOver = false;
+                }
+            }
+        EndDrawing();
+    }
+    CloseWindow();
+    
+    return 0;
+}
